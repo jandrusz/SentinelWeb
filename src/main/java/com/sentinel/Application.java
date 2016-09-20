b2b4ce2eea9371db3aa2498291ccc2ffdcf1623e
jandruszPC <JAndruszek@gmail.com>
2016-09-20 21:39:55 +0200

    Teraz apliakcja uruchamia sie na tomcacie.

--- src/main/java/com/sentinel/Application.java
+++ src/main/java/com/sentinel/Application.java
@@ -1,11 +1,23 @@
 package com.sentinel;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
-public class Application {
+@EnableAutoConfiguration
+@ComponentScan
+public class Application extends SpringBootServletInitializer {
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        new Application().configure(new SpringApplicationBuilder(Application.class)).run(args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
     }
 }
